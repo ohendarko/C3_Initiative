77439ee4e10e4d7ff0d0b8227ff2bbe9f71ca8c6
ohendarko <ohendarko@gmail.com>
2026-01-08 01:38:20 -0500

    moved routes out of learner gates

---
+++ app/(visitor)/layout.tsx
@@ -0,0 +1,30 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "../globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeToggle } from "@/components/theme-toggle"
+import SessionWrapper from "@/providers/SessionWrapper"
+
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "C3 Initiative - Smart Cervical Cancer Education",
+  description: "C3 Initiative's premium e-learning platform for cervical cancer education and awareness",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <SessionWrapper>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          <ThemeToggle />
+          {children}
+        </ThemeProvider>
+    </SessionWrapper>
+  )
+}
